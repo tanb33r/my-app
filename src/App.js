f376eacea6e8c53f733837cdc02a80cbe6637a74
tanb33r <tanbeer.taher@gmail.com>
2022-08-05 18:21:00 +0600

    3

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ var placeFormData = [
 // var placeFormData = data;
 
 const App = () => {
+    let [changeText, setChangeText] = useState(true);
+    const handleChange = () => {
+        return setChangeText(!changeText);
+    };
 
     const [places, setPlaces] = useState(placeFormData);
     const [addFormData, setAddFormData] = useState({
@@ -58,16 +62,43 @@ const App = () => {
     }
     let toggle = 0;
 
+    return (
+        <div>
+            <button onClick={() => handleChange()}>click me</button>
+            {changeText ?  <PlaceTable {...props} /> :  <div className="app-container">
+                <h2>Add Place</h2>
+                <form onSubmit={handleAddFormSubmit}>
+                    Name :<input
+                        type="text"
+                        name="placeName"
+                        required='required'
+                        onChange={handleAddFormChange} />
 
+                    Address :<input
+                        type="text"
+                        name="address"
+                        id="address"
+                        onChange={handleAddFormChange} />
+
+                    Rating (1-5):<input
+                        type="number"
+                        name="rating"
+                        id="rating"
+                        min="1" max="5"
+                        onChange={handleAddFormChange} />
+
+                    {/* Picture:<br></br>
+                <input type="file" id="imgPlace" onchange="previewFile()" />
+                <br>
+                </br> */}
+                    <button type="submit">Add</button>
+                </form>
+            </div>}
+        </div>
+    );
+
+  
     if (toggle === 0) {
-        return (
-            <div className="app-container">
-                <PlaceTable {...props} />
-
-            </div>
-        );
-    }
-     if (toggle === 0) {
         return (
             <div className="app-container">
                 <h2>Add Place</h2>
@@ -99,7 +130,7 @@ const App = () => {
                 </form>
             </div>
         );
-    }
-}
 
-export default App;
+    }}
+
+    export default App;
